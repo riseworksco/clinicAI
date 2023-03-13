be9f9da90ec78cb81a37ae6fb5831d9a535ebeaf
Yuan Chen <18861720589@163.com>
2023-03-13 16:20:55 -0400

    feat(django): init django project

--- Music_Therapy_API/settings.py
+++ Music_Therapy_API/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
