a1fdd59dc8f7483a4cf7c95d3b949dc91a3287c7
Yuan Chen <cysbc1999@gmail.com>
2024-02-02 15:40:40 -0500

    Update Music_Therapy_API/settings.py

--- Music_Therapy_API/settings.py
+++ Music_Therapy_API/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-# import django_heroku
+import django_heroku
 import cloudinary
 
 from pathlib import Path
