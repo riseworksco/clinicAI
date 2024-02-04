28697d6acf6e4b2217586a2f851a0d1828cf1308
Yuan Chen <cysbc1999@gmail.com>
2024-02-04 11:10:11 -0500

    fix(footer): fix some footer mismatch

--- findatherapist/urls.py
+++ findatherapist/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 
+app_name = 'findatherapist'
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
