1636819a7bba8f2fa78e27ab7c621af4a03eeb44
Yuan Chen <cysbc1999@gmail.com>
2024-02-23 20:21:50 -0500

    feat(admin): add a few assessment models to admin.py

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stomp
+
 
 # Register your models here.
+
+class StompAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Stomp, StompAdmin)
--- sheetGAD/admin.py
+++ sheetGAD/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import GAD7Submission
+
 
 # Register your models here.
+
+class GAD7SubmissionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GAD7Submission, GAD7SubmissionAdmin)
