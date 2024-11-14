07e1ef3b273fb38f8754362fde21dab6cf569421
Yuan Chen <cysbc1999@gmail.com>
2024-11-14 18:01:37 -0500

    bugfix database rotation

--- Music_Therapy_API/settings.py
+++ Music_Therapy_API/settings.py
@@ -110,12 +110,12 @@ WSGI_APPLICATION = "Music_Therapy_API.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
